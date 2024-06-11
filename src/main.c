e9c3314070748531e6d3d477ab9496f9a3f63bfc
aautin <ezagof@gmail.com>
2024-06-11 10:05:06 +0200

    init mlx, get the screen's size and check it

--- src/main.c
+++ src/main.c
@@ -6,14 +6,30 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 23:44:49 by aautin            #+#    #+#             */
-/*   Updated: 2024/06/11 22:03:45 by aautin           ###   ########.fr       */
+/*   Updated: 2024/06/12 14:43:11 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+#include "mlx.h"
+#include "mlx_int.h"
+
+#define DIR_LENGHT				-10
+#define PLANE_LENGHT			10
 
 int	main(int argc, char **argv)
 {
 	(void) argc;
 	(void) argv;
-	
-	return (0);
+
+	void *mlx = mlx_init();
+	if (mlx == NULL)
+		return (1);
+
+	int	xScreenSize, yScreenSize;
+	mlx_get_screen_size(mlx, &xScreenSize, &yScreenSize);
+	if (xScreenSize <= DIR_LENGHT * 2 || yScreenSize <= DIR_LENGHT * 2)
+		return 0;
+
+	return 0;
 }
