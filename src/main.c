7502601a49d27b2e11ddf1a3de532a6a57c12fda
aautin <ezagof@gmail.com>
2024-06-13 18:47:35 +0200

    explicited variables, set dir and plane lenghts on 90FOV

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 23:44:49 by aautin            #+#    #+#             */
-/*   Updated: 2024/06/13 00:19:00 by aautin           ###   ########.fr       */
+/*   Updated: 2024/06/13 19:04:15 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 #include "mlx.h"
 #include "mlx_int.h"
 
-#define DIR_LENGHT				-10
-#define PLANE_LENGHT			10
+#define DIR_LENGHT				1
+#define PLANE_LENGHT			2 * DIR_LENGHT
+// here "2 * DIR_LENGHT" sets a 90' FOV
 
 #define DESTROY_WINDOW_EVENT	17
 #define DESTROY_WINDOW_MASK		0
@@ -25,29 +26,29 @@
 
 #define ESC_KEYCODE				65307
 
-#define	A_KEYCODE				97
-#define	D_KEYCODE				100
-#define	S_KEYCODE				115
-#define	W_KEYCODE				119
+#define	A_KEYCODE_LEFT			97
+#define	D_KEYCODE_RIGHT			100
+#define	S_KEYCODE_DOWN			115
+#define	W_KEYCODE_UP			119
 
 #define	MLX_INDEX				0
 #define	PLAYER_XPOS_INDEX		1
 #define	PLAYER_YPOS_INDEX		2
 
-#define SCREEN_XSIZE_INDEX		3
-#define SCREEN_YSIZE_INDEX		4
+#define WINDOW_XSIZE_INDEX		3
+#define WINDOW_YSIZE_INDEX		4
 
-void	modifyPlayerPos(int keycode, void **params)
+void	modifyPlayerPosition(int keycode, void **params)
 {
-	int *xPlayerPos = params[PLAYER_XPOS_INDEX];
-	int *yPlayerPos = params[PLAYER_YPOS_INDEX];
-	int *xScreenSize = params[SCREEN_XSIZE_INDEX];
-	int *yScreenSize = params[SCREEN_YSIZE_INDEX];
+	int *xPlayerPosition = params[PLAYER_XPOS_INDEX];
+	int *yPlayerPosition = params[PLAYER_YPOS_INDEX];
+	int *xScreenSize = params[WINDOW_XSIZE_INDEX];
+	int *yScreenSize = params[WINDOW_YSIZE_INDEX];
 
-	*xPlayerPos -= (keycode == A_KEYCODE && *xPlayerPos > 0);
-	*xPlayerPos += (keycode == D_KEYCODE && *xPlayerPos < *xScreenSize);
-	*yPlayerPos -= (keycode == W_KEYCODE && *yPlayerPos > 0);
-	*yPlayerPos += (keycode == S_KEYCODE && *yPlayerPos < *yScreenSize);
+	*xPlayerPosition -= (keycode == A_KEYCODE && *xPlayerPosition > 0);
+	*xPlayerPosition += (keycode == D_KEYCODE && *xPlayerPosition < *xScreenSize);
+	*yPlayerPosition -= (keycode == W_KEYCODE && *yPlayerPosition > 0);
+	*yPlayerPosition += (keycode == S_KEYCODE && *yPlayerPosition < *yScreenSize);
 }
 
 int	keyHandlerEvent(int keycode, void **params)
@@ -57,7 +58,7 @@ int	keyHandlerEvent(int keycode, void **params)
 		mlx_loop_end(params[MLX_INDEX]);
 	else
 	{
-		modifyPlayerPos(keycode, params);
+		modifyPlayerPosition(keycode, params);
 		ft_printf("player's position: %dx*%dy\n",
 				* (int *)params[PLAYER_XPOS_INDEX], * (int *)params[PLAYER_YPOS_INDEX]);
 	}
@@ -78,16 +79,16 @@ int	main(int argc, char **argv)
 	if (xScreenSize <= DIR_LENGHT * 2 || yScreenSize <= DIR_LENGHT * 2)
 		return 0;
 
-	int	xPlayerPos = xScreenSize / 2, yPlayerPos = yScreenSize / 2;
+	int	xPlayerPosition = xScreenSize / 2, yPlayerPosition = yScreenSize / 2;
 
 	void *window = mlx_new_window(mlx, xScreenSize, yScreenSize, "cub3d: top_view");
 
 	void *params[5];
 	params[MLX_INDEX] = mlx;
-	params[PLAYER_XPOS_INDEX] = &xPlayerPos;
-	params[PLAYER_YPOS_INDEX] = &yPlayerPos;
-	params[SCREEN_XSIZE_INDEX] = &xScreenSize;
-	params[SCREEN_YSIZE_INDEX] = &yScreenSize;
+	params[PLAYER_XPOS_INDEX] = &xPlayerPosition;
+	params[PLAYER_YPOS_INDEX] = &yPlayerPosition;
+	params[WINDOW_XSIZE_INDEX] = &xScreenSize;
+	params[WINDOW_YSIZE_INDEX] = &yScreenSize;
 
 	mlx_hook(window, DESTROY_WINDOW_EVENT, DESTROY_WINDOW_MASK, &mlx_loop_end, params);
 	mlx_hook(window, KEY_PRESSED_EVENT, KEY_PRESSED_MASK, &keyHandlerEvent, params);
