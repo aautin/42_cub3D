c71345a85fb5a8024b994b2a25b205253c2331c2
ezagof_pc <ezagof@gmail.com>
2024-06-18 19:08:48 +0200

    identifyMap(), which detects the 3 different cases
    a line which identifies:
        - a texture,
        - a code,
        - the area.

--- src/identifiedMap.c
+++ src/identifiedMap.c
@@ -6,13 +6,15 @@
 /*   By: alexandre <alexandre@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/16 23:37:02 by alexandre         #+#    #+#             */
-/*   Updated: 2024/06/18 14:46:52 by alexandre        ###   ########.fr       */
+/*   Updated: 2024/06/18 19:08:29 by alexandre        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "get_next_line.h"
 #include "map.h"
+
+#include "ft_printf.h"
 
 static int	initDataBlock(char *dataBlock[], int fd)
 {
@@ -40,10 +42,35 @@ static int	initDataBlock(char *dataBlock[], int fd)
 	return EXIT_SUCCESS;
 }
 
-int	initIdentifiedMap(t_identifiedMap *identifiedMap, char *mapFileName)
+static int	identifyMap(t_identifiedMap *map, char **lines)
 {
-	(void) identifiedMap;
+	int	status = 0;
+	int	i = 0;
+	while (lines[i] != NULL) {
+		int	j = 0;
+		int	newStatus = 0;
+		while (lines[i][j] == ' ')
+			i++;
+		if ((lines[i][j] == 'F' || lines[i][j] == 'C') 
+			&& lines[i][j + 1] == ' ')
+			newStatus = identifyCode(map, &lines[i][j + 1]);
+		else if (ft_strnstr(&lines[i][j], "NO ", 3)
+			|| ft_strnstr(&lines[i][j], "SO ", 3)
+			|| ft_strnstr(&lines[i][j], "WE ", 3)
+			|| ft_strnstr(&lines[i][j], "EA ", 3))
+			newStatus = identifyTexture(map, &lines[i][j]);
+		else if (ft_strchr("10", lines[i][j]))
+			newStatus = identifyArea(map, &lines[i]);
+		if (newStatus == 0)
+			break;
+		status |= newStatus;
+		i++;
+	}
+	return status;
+}	
 
+int	initIdentifiedMap(t_identifiedMap *map, char *mapFileName)
+{
 	int fd = open(mapFileName, O_RDONLY);
 	if (fd == -1)
 	{
@@ -54,6 +81,10 @@ int	initIdentifiedMap(t_identifiedMap *identifiedMap, char *mapFileName)
 	char *dataBlock;
 	if (initDataBlock(&dataBlock, fd) == EXIT_FAILURE)
 		return EXIT_FAILURE;
-	free(dataBlock);	
+
+	char	**lines = ft_split(dataBlock, '\n');
+	int		status = identifyMap(map, lines);
+	free(dataBlock);
+	free_stab(lines);
 	return EXIT_SUCCESS;
 }
