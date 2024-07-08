491ecbbb16b70cde7ffd5ecf53c67b03378b57a8
eliott norie <eliott.norie@gmail.com>
2024-07-08 14:05:31 +0200

    added slide when colliding with wall

--- enorie/move.c
+++ enorie/move.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 13:15:22 by root              #+#    #+#             */
-/*   Updated: 2024/07/08 14:02:53 by root             ###   ########.fr       */
+/*   Updated: 2024/07/08 14:05:23 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,6 @@ void	ft_move_left(t_all *all)
 	newposx = all->pos->x + all->pos->diry * 0.05;
 	newposy = all->pos->y - all->pos->dirx * 0.05;
 	if (all->map->map[(int)newposy][(int)newposx] != '1')
-		if (all->map->map[(int)all->pos->y][(int)newposx] != '1')
 		all->pos->x = newposx;
 	if (all->map->map[(int)newposy][(int)all->pos->x] != '1')
     	all->pos->y = newposy;
